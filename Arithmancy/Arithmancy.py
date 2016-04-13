150a9a888168291d47c2be8d1a6355a4c7b4039a
Tom McClintock <tmcclintock@email.arizona.edu>
2016-04-13 08:39:44 -0700

    Arithmancy program completed.

---
+++ Arithmancy/Arithmancy.py
@@ -0,0 +1,66 @@
+"""
+This file contains the Arithmancy object, which contains all the capabilities of
+solving the Pathfinder Arithmancy, as detailed on the page:
+
+http://www.d20pfsrd.com/feats/general-feats/arithmancy
+
+Essentially the user enters a phrase that respresents a spell.
+Each letter is assigned a numeric value and then summed together.
+The digits of the result are then summed and this process is
+repeated until there is only
+a single digit left. This is called the 'digital root' of the
+spell, and is used in the game.
+
+Note that it is true that the digital root (DR) of two words
+is equal to the digital roots of the words added together.
+Thus:
+
+DR(wordone wordtwo) = DR(DR(wordone) + DR(wordtwo))
+
+This can be shown algebraically, but is too long
+for this comment.
+"""
+
+class Arithmancy(object):
+    def __init__(self,phrase='',debug=False):
+        self.phrase = phrase
+        self.reduced_phrase = self.phrase.lower().replace(" ","")
+        self.debug = debug #Used to print extra stuff
+        self.root = self.calc_root()
+
+    """
+    This is the formula used to calculate the numerical
+    value of each letter in the alphabet. For letters a-i
+    it is numbers 1-9, and then it wraps back around to 1.
+    z has the value 8.
+    """    
+    def letter_root(self,char):
+        return (ord(char)-ord('a'))%9+1
+
+    def calc_root(self):
+        rp = self.reduced_phrase
+        total = 0
+        for char in rp:
+            total+=self.letter_root(char)
+            if self.debug:
+                print char,self.letter_root(char)
+        if self.debug:
+            print "temp total first = ",total
+        while total > 9:
+            digits = [int(char) for char in str(total)]
+            newtotal = 0
+            for d in digits:
+                newtotal += d
+            total = newtotal
+            if self.debug:
+                print "temp total = ",total,'digits = ',digits
+        return total
+
+    def __str__(self):
+        return "%s : digital root = %d"%(self.phrase,self.root)
+
+#A unit test
+if __name__ == '__main__':
+    spell = 'Empowered Fireball'
+    test = Arithmancy(spell)
+    print test
