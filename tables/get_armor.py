241450031b2238abd392b3d8df3a4175a28948fd
Tom McClintock <tmcclintock89@gmail.com>
2017-03-16 07:01:22 -0700

    Outlined the algorithm in get_armor.py

--- tables/get_armor.py
+++ tables/get_armor.py
@@ -1,5 +1,13 @@
 """
 Use beautiful soup to get the armor table.
+
+Algorithm is as follows:
+1. get all the links on the armor page
+2. figure out all the armor names from the links and keep them both
+3. navigate to each armor's page and find the cost (tricky)
+4. sort on the costs
+5. print the whole thing to a text file with the entry style: Name Cost URL
+
 """
 import requests
 from bs4 import BeautifulSoup as bs
