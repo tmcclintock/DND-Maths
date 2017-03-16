b73c27e916f99b8d69ea395a36c402491137e395
Tom McClintock <tmcclintock89@gmail.com>
2017-03-16 07:57:07 -0700

    Successfully getting the armors out individually. Just need the costs.

--- tables/get_armor.py
+++ tables/get_armor.py
@@ -11,11 +11,45 @@ Algorithm is as follows:
 """
 import requests
 from bs4 import BeautifulSoup as bs
+import sys
 
-url = "http://www.d20pfsrd.com/magic-items/magic-armor/specific-magic-armor/"
-r = requests.get(url)
+class item(object):
+    def __init__(self,name,cost=-1,url=""):
+        self.name=name
+        self.cost=cost
+        self.url=url
+
+    def __str__(self):
+        return "%s\t%d\t%s"%(self.name,self.cost,self.url)
+
+def get_name(text):
+    """Given the name in the URL with hyphens and stuff get the real name.
+    """
+    text = text.replace('/','')
+    text = text.replace('-s-','\'s ')
+    text = text.replace('-',' ')
+    return text
+
+base = "http://www.d20pfsrd.com/magic-items/magic-armor/specific-magic-armor/"
+r = requests.get(base)
 data = r.text
 soup = bs(data,'lxml')
+armors = []
+names = []
 
+#Assemble a list of all the names and urls
 for link in soup.find_all('a'):
-    print link.get('href')
+    url = link.get('href')
+    if url.startswith(base) and len(url) > len(base):
+        name =  get_name(url[len(base):])
+        if name not in names: 
+            names.append(name)
+            armors.append(item(name,url=url))
+            print armors[-1]
+
+#Loop through the armors and get the costs
+for armor in armors:
+    soup = bs(requests.get(armor.url).text,'lxml')
+    print type(soup)
+    sys.exit()
+    
